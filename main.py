91d5c73d12bbf4f451355a617a331533332863e0
Steven Serrano <steven.serrano@tup.edu.ph>
2021-10-23 14:21:02 +0800

    add file handling

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 #ENCRYPTING METHOD USING INCREMENTING ASCII 
-def encrypt(passedString,key):
-    encryptedString = ""
-    for ch in passedString:
-        encryptedString = encryptedString + chr(ord(ch)+key)
+def encrypt(passed_string,key,location):
+    encrypted_string = ""
+    for ch in passed_string:
+            encrypted_string = encrypted_string + chr(ord(ch)+key) 
 
-    return encryptedString
+    # print("ECRYPTED: ",encrypted_string)        
+    with open(location,'w+',encoding="utf-8") as f:  
+        f.writelines(encrypted_string)
+        f.seek(0)
+        print("ENCRPYTED: ",f.readline())
 
-def decrypt(passedString,key):
-    decryptedString = ""
-    for ch in passedString:
-        decryptedString = decryptedString + chr(ord(ch)-key)
 
-    return decryptedString
+def decrypt(key,location):
+    decrypted_string = ""
+    with open(location,'r',encoding="utf-8") as f:
+        file_content = f.readline()
+
+    for ch in file_content:
+        decrypted_string = decrypted_string + chr(ord(ch)-key)
+
+    return decrypted_string
+
 
 def main():
-    myKey=29
-    myString = encrypt("abc def ghi jkl",myKey)
-    print(myString)
-    print(decrypt(myString,myKey))
+    my_key=29
+    location = 'internet.txt'
+    encrypt("abc def ghi 6kl",my_key,location)
+    decrypted_string = decrypt(my_key,location)
+    print("DECRYPTED: ",decrypted_string)
 
 if __name__ == "__main__":
     main()
