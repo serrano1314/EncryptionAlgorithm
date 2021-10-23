0badfd3902c15a4f2e723044f799c4e77626a28f
Steven Serrano <steven.serrano@tup.edu.ph>
2021-10-23 13:05:59 +0800

    add encrypt and decrypt function

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-def encrypt():
-    pass
+#ENCRYPTING METHOD USING INCREMENTING ASCII 
+def encrypt(passedString,key):
+    encryptedString = ""
+    for ch in passedString:
+        encryptedString = encryptedString + chr(ord(ch)+key)
 
-def decrypt():
-    pass
+    return encryptedString
 
+def decrypt(passedString,key):
+    decryptedString = ""
+    for ch in passedString:
+        decryptedString = decryptedString + chr(ord(ch)-key)
+
+    return decryptedString
 
 def main():
-    myString = "abc def ghi jkl"
-    encryptedString = ""
-    for ch in myString:
-        encryptedString = encryptedString + chr(ord(ch)+1)
-    
-    print(encryptedString)
+    myKey=29
+    myString = encrypt("abc def ghi jkl",myKey)
+    print(myString)
+    print(decrypt(myString,myKey))
 
 if __name__ == "__main__":
     main()
